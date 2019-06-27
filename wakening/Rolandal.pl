0499c57f16c6883e1f4d3fece432172f37caa7eb
Sulien <sulien.eq@gmail.com>
2019-06-27 00:23:54 +0000

    Fix a bug preventing the Circlet of Kalinkan quest from being completable.

--- wakening/Rolandal.pl
+++ wakening/Rolandal.pl
@@ -3,25 +3,20 @@ sub EVENT_SAY {
     if ($text=~/hail/i) {
       quest::say("Greetings, Traveler. I do not receive many visitors to my quarters here, besides the occasional unfortunate treasure seeker that often will make for a good snack.");
     }
-    if ($CircletFalinkan=="1") { # we did the part with Karkador
-      if ($text=~/kardakor.*sent.*me/i) {
-        quest::say("You are sent from Kardakor are you? Well surely he must have sent you to stand before me for a reason.");
-      }
-      if ($text=~/talisman.*identif/i) {
-        quest::say("Ah of course, It has been quite a long time sense my powers of identification have been requested. If you are sent from Kardakor then your intent must be well directed. I do have a favor to ask of you before I identify your talisman. I seek some rare treasures to further some studies that I have been working on for a long time.");
-      }
-      if ($text=~/rare.*treasures/i) {
-        quest::say("There are 3 items that I seek to continue my studies of some various and musterious magics. Bring to me the teachings of a high ranking Kromzek that I hear goes by the name of Gkrean, a chipped fang from a beast deep within the Cursed Necropolis. Also for good measure, I require the Head of a Kromzek Staff Sergeant to prove to your dedication to our cause. Present these 3 items to me along with your Talisman that you wish to learn more about and I shall do my best to identify its origin for you.");
-      }
+    if ($text=~/kardakor.*sent.*me/i) {
+      quest::say("You are sent from Kardakor are you? Well surely he must have sent you to stand before me for a reason.");
+    }
+    if ($text=~/talisman.*identif/i) {
+      quest::say("Ah of course, It has been quite a long time sense my powers of identification have been requested. If you are sent from Kardakor then your intent must be well directed. I do have a favor to ask of you before I identify your talisman. I seek some rare treasures to further some studies that I have been working on for a long time.");
+    }
+    if ($text=~/rare.*treasures/i) {
+      quest::say("There are 3 items that I seek to continue my studies of some various and musterious magics. Bring to me the teachings of a high ranking Kromzek that I hear goes by the name of Gkrean, a chipped fang from a beast deep within the Cursed Necropolis. Also for good measure, I require the Head of a Kromzek Staff Sergeant to prove to your dedication to our cause. Present these 3 items to me along with your Talisman that you wish to learn more about and I shall do my best to identify its origin for you.");
     }
   }
 }
 
 sub EVENT_ITEM {
   if ($faction==1) {
- 
-    if ($CircletFalinkan=="1") {
-
       # 1861 : An old worn Talisman
       # 1864 : Teaching of Gkrean
       # 1865 : Head of Staff Sergeant Drioc
@@ -41,7 +36,6 @@ sub EVENT_ITEM {
         quest::say("It would be my guess that you present this to me in hopes of finding out more about the magics it possesses. Before I can do this I require some rare treasures that you must present to me.");
         quest::summonitem(1861); # you get it back
       }
-    }
   }
   plugin::return_items(\%itemcount); # return unused items
 }
