9def2f76d5420b3552b7794f8b3d2e25e3b981b9
Justin Wienckowski <jwinky@gmail.com>
2019-06-26 17:12:09 -0700

    Disabling an exploit fix that is causing Jaled'Dar to sometimes aggro on PCs in the Chetari tunnels.

--- necropolis/A_Guardian_of_the_Shrine.pl
+++ necropolis/A_Guardian_of_the_Shrine.pl
@@ -7,13 +7,16 @@ sub EVENT_SPAWN {
   quest::set_proximity($x - 7, $x + 7, $y - 7, $y + 7);
 }
 
-sub EVENT_ENTER {
-  $cheater = $entity_list->GetMobByNpcTypeID(123011);
-  if ($cheater) {
-    $addcheater = $cheater->CastToNPC();
-    $addcheater->AddToHateList($client, 1);
-  }
-}
+# Disabled 2019-Jun by Sulien because this is causing Jaled'Dar to aggro
+# on PCs hunting in the rat tunnels.
+
+#sub EVENT_ENTER {
+#  $cheater = $entity_list->GetMobByNpcTypeID(123011);
+#  if ($cheater) {
+#    $addcheater = $cheater->CastToNPC();
+#    $addcheater->AddToHateList($client, 1);
+#  }
+#}
 
 # EOF NPC: A_Guardian_of_the_Shrine
 
